c3913976908c0f875fb64c4a06d63be1ad9ba45a
Tan, Zhuo <Zhuo.Tan@dgs.ca.gov>
2023-03-02 09:51:54 -0800

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,9 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //DefaultConnection defined in from appsettings.json file
 });
+
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
